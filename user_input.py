ea868a9e72aef5965f2ecfa6896668f8dd71d4dd
Oliv-js <73039291+Oliv-js@users.noreply.github.com>
2024-02-26 00:52:07 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("what is your name?")
+age = input("what's your age?")
+location = input("whats your location?")
+print("Hello",name, "you are",age,"years old and you live in",location,".")
